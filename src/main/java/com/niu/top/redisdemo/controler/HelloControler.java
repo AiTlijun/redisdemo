48193be0db8dd36013592eec4c99b93220001f61
TS.SZX.szlijun <szlijun@travelsky.com>
2018-11-09 14:20:10 +0800

    Jenkins test

---
+++ src/main/java/com/niu/top/redisdemo/controler/HelloControler.java
@@ -0,0 +1,18 @@
+package com.niu.top.redisdemo.controler;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author hongwei
+ * @date 2018/11/9 14:14
+ */
+@RestController
+public class HelloControler {
+
+    @RequestMapping("/hello")
+    public String hello(){
+
+        return "hello wolder!";
+    }
+}
